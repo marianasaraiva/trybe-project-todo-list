a992b260dda8b024bbb1bfff59dc029b93881ace
Mariana Saraiva <marianascm8@gmail.com>
2021-09-13 15:13:14 -0300

    Requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Aqui')
+const paragraphyID = document.getElementById('funcionament');
+const inputTextTarefa = document.getElementById('texto-tarefa');
+const olTarefa = document.getElementById('lista-tarefas')
+const buttonCreateTarefa = document.getElementById('criar-tarefa');
+
+buttonCreateTarefa.addEventListener('click', function(){
+  let elementLi = document.createElement('li');
+  elementLi.innerText = inputTextTarefa.value;
+  olTarefa.appendChild(elementLi);
+  inputTextTarefa.value = "";
+})
