9a1c158661ac1a55234474b017102143194807fa
Mariana Saraiva <marianascm8@gmail.com>
2021-09-13 15:45:38 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const paragraphyID = document.getElementById('funcionament');
 const inputTextTarefa = document.getElementById('texto-tarefa');
 const olTarefa = document.getElementById('lista-tarefas')
 const buttonCreateTarefa = document.getElementById('criar-tarefa');
+const liList = document.getElementsByClassName('item-list');
+console.log(liList);
 
 buttonCreateTarefa.addEventListener('click', function(){
   let elementLi = document.createElement('li');
   elementLi.innerText = inputTextTarefa.value;
+  elementLi.classList.add('item-list');
   olTarefa.appendChild(elementLi);
   inputTextTarefa.value = "";
+})
+
+olTarefa.addEventListener('click', function(event){
+  for (let index = 0; index < liList.length; index += 1){
+    liList[index].style.backgroundColor = 'rgb(128, 128, 128)';
+    event.target.classList.toggle('selected');
+  }
 })
